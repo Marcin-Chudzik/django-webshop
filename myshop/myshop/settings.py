560919dba9930d11e93295da510ea8703ee29059
Marcin_Chudzik <marcinchudzik.pythondev@gmail.com>
2023-03-02 17:30:40 +0100

    update bootstrap and added details template
    updated bootstrap to 5.2.3
    added a product details view template.

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,3 +124,7 @@ STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
