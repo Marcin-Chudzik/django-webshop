4d6beb5374bb7b07f9766592de3eac6564cd666f
Marcin Chudzik <marcinchudzik.webdeveloper@gmail.com>
2023-03-12 14:55:14 +0100

    add item to the cart form and cart views
    - add item to the cart form.
    - add item to the cart view.
    - remove item from the cart view.
    - cart detail view.

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from django.contrib.sessions.backends.base import SessionBase
-from myshop.shop.models import Product
+from shop.models import Product
 
 
 class Cart:
--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods
+from shop.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
 
-# Create your views here.
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
+
+
+@require_http_methods(['POST'])
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
+    return redirect('cart:cart_detail')
+
+
+@require_http_methods(['POST'])
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
